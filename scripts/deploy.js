60d8dfb2b8705d1b6051193f8f1937491c898127
alexander <alexander.lechner@outlook.com>
2021-12-07 13:51:23 +0100

    enable deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,11 +6,9 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
-    const helloWorldContract = await HelloWorld.attach('0x6db6da1256AeD3b5e41c9D1CfAb586013cfdd863')
-    console.log("Current message: ", helloWorldContract.message())
-    // const helloWorldContract = await HelloWorld.deploy("Hello World!");
+    const helloWorldContract = await HelloWorld.deploy("Hello World!");
 
-    // console.log("HelloWorld address:", helloWorldContract.address);
+    console.log("HelloWorld address:", helloWorldContract.address);
 }
 
 main()
--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -1,6 +1,7 @@
 async function main() {
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
     const helloWorldContract = await HelloWorld.attach('0x6db6da1256AeD3b5e41c9D1CfAb586013cfdd863')
+    
     console.log("Current message:", await helloWorldContract.message())
 }
 
